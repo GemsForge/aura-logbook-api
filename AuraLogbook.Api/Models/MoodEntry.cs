cb4765f68d79db99b4b35f849f15d0860d92bb8e
Gem Code <114959173+GemsForge@users.noreply.github.com>
2025-06-15 01:52:36 -0400

    feat(mood): #6 create mood entry model and model type.

---
+++ AuraLogbook.Api/Models/MoodEntry.cs
@@ -0,0 +1,16 @@
+﻿using AuraLogbook.Api.Models.Enums;
+using System.Text.Json.Serialization;
+
+namespace AuraLogbook.Api.Models;
+public class MoodEntry
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public DateTime Date { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public List<MoodType> Moods { get; set; } = new();
+
+    public string? Comment { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
