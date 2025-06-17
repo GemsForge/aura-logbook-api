b9dfbe44a2885dc93aa8e996563c9e703e15ff65
Gem Code <114959173+GemsForge@users.noreply.github.com>
2025-06-17 12:35:38 -0400

    chore(auth): #59 handle state of token in localStorage at login and logout

--- aura-ui/src/store/slices/authSlice.ts
+++ aura-ui/src/store/slices/authSlice.ts
@@ -1,5 +1,5 @@
 // src/store/authSlice.ts
-import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
   token: string | null;
@@ -7,25 +7,35 @@ interface AuthState {
   userEmail: string | null;
 }
 
+const token = localStorage.getItem("token");
+const email = localStorage.getItem("userEmail");
+
 const initialState: AuthState = {
-  token: null,
-  isAuthenticated: false,
-  userEmail: null,
+  token,
+  isAuthenticated: !!token,
+  userEmail: email,
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
-    loginSuccess(state, action: PayloadAction<{ token: string; email: string }>) {
+    loginSuccess(
+      state,
+      action: PayloadAction<{ token: string; email: string }>
+    ) {
       state.token = action.payload.token;
       state.userEmail = action.payload.email;
       state.isAuthenticated = true;
+      localStorage.setItem("token", token!);
+      localStorage.setItem("userEmail", email!);
     },
     logout(state) {
       state.token = null;
       state.userEmail = null;
       state.isAuthenticated = false;
+      localStorage.removeItem("token");
+      localStorage.removeItem("userEmail");
     },
   },
 });
